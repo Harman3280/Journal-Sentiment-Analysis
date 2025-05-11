2591f5f8762adbf5a60fd4e855c69a3c719410f0
Harman Singh <harman.singh@peoplestrong.com>
2025-05-11 16:22:39 +0530

    JWT Implemented in Journal app

--- src/main/java/org/example/config/SpringSecurity.java
+++ src/main/java/org/example/config/SpringSecurity.java
@@ -1,9 +1,11 @@
 package org.example.config;
 
+import org.example.filter.JwtFilter;
 import org.example.service.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -15,6 +17,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -29,7 +32,11 @@ public class SpringSecurity {
     */
 
     @Autowired
+    @Lazy
     UserDetailsServiceImpl userDetailsService;
+
+    @Autowired
+    JwtFilter jwtFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -43,19 +50,16 @@ public class SpringSecurity {
                         .antMatchers("/admin/**").hasRole("ADMIN") //RoleBasedAuthentication
                         //.anyRequest().authenticated())
                         .anyRequest().permitAll())
-                .httpBasic(Customizer.withDefaults())   // Enable basic authentication
+                //.httpBasic(Customizer.withDefaults())   // Disabling basic authentication
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .csrf(AbstractHttpConfigurer::disable)  // Disable CSRF if using APIs
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
 
                 .build();
     }
 
 
-    @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
-        return authConfig.getAuthenticationManager();
-    }
 
 
     // This Encoder will take a string and generate a HASH for randomized string
@@ -63,6 +67,15 @@ public class SpringSecurity {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+
+
+    // Bean created here because we are using its instance in login method in PublicController
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+        return authConfig.getAuthenticationManager();
+    }
+
 
 
 }
